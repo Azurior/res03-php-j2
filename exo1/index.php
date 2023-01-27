9b80c0b49d73a46c092e2948ac0d74cb49aad8e5
Azurior <azurior24@gmail.com>
2023-01-27 09:56:41 +0000

    require

---
+++ exo1/index.php
@@ -0,0 +1,18 @@
+<?php 
+
+$form = array(
+    "nom" => $_POST['nom'],
+    "prenom" => $_POST['prenom'],
+    "email" => $_POST['email'],
+    "telephone" => $_POST['telephone'],
+    "linkedin" => $_POST['linkedin'],
+    "github" => $_POST['github'],
+    "biographie" => $_POST['biographie'],
+    "loisir-1" => $_POST['loisir-1'],
+    "loisir-2" => $_POST['loisir-2'],
+    "loisir-3" => $_POST['loisir-3'],
+    "loisir-4" => $_POST['loisir-4'],
+    
+    );
+
+?>
